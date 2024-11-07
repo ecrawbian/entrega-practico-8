d325af54029555cf3a2c18a6116211f389570871
ecrawbian <caballero-erwin@hotmail.es>
2024-11-07 17:45:11 -0300

    Create 5. CartItem.jsx

---
+++ 5. CartItem.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+function CartItem({ item, removeFromCart }) {
+  return (
+    <div className="cart-item" onClick={() => removeFromCart(item.id)}>
+      {item.name} - ${item.unitPrice.toFixed(2)} x {item.quantity}
+    </div>
+  );
+}
+
+export default CartItem;
